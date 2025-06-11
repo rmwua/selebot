887f28f6fc5f7459d3865d66e2a022073e6d6a70
rmwua <meloniel@yandex.ru>
2025-06-11 21:45:45 +0300

    update shell

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,13 @@
 #!/bin/sh
 set -e
 
+cd /app
+
 echo ">>> Running migrations, DATABASE_URL=$DATABASE_URL"
+echo ">>> Current directory: $(pwd)"
+echo ">>> Listing files:"
+ls -l
+
 alembic current
 alembic upgrade head
 
