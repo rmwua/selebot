1ae42c62fc240d42076312d4516cf5ff3757054c
rmwua <meloniel@yandex.ru>
2025-07-03 16:38:12 +0300

    entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,12 @@
-#!/bin/sh
 set -e
 
 cd /app
+
+mkdir -p /.keys
+if [ -n "$GOOGLE_SA_KEY_B64" ]; then
+  echo "$GOOGLE_SA_KEY_B64" | base64 -d > /app/.keys/sa.json
+  export GOOGLE_SA_KEY_PATH=/app/.keys/sa.json
+fi
 
 echo ">>> Running migrations, DATABASE_URL=$DATABASE_URL"
 echo ">>> Current directory: $(pwd)"
